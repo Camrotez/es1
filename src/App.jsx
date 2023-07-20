0a740d5c2cd92268592c775e00a17088e88a7c38
Luciano <lunick997@gmail.com>
2023-07-20 14:27:51 +0200

    commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Title from "./assets/Title"
+
+
+function App() {
+
+  return(
+    <Title/>
+  )
+}
+
+export default App
---
+++ src/assets/Title.jsx
@@ -0,0 +1,5 @@
+function Title(){
+    return <h1>hello!</h1>
+}
+
+export default Title
